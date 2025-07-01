87b67f00b3f1ca7cdef23ba0ede486774a53b8c4
Danilo <danilo.carrillo02@inacapmail.cl>
2025-06-30 20:44:23 -0400

    Ajuste y configuración de Git

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import FormularioCompra from './components/FormularioCompra'; // Importa el componente
 
+// src/App.jsx
 function App() {
-
   return (
     <Container className="mt-5">
       <h1>Control de Compras Éticas</h1>
+      {/* Configuración de Git corregida */}
       <hr />
-      <FormularioCompra /> {/* Usa el componente aquí */}
+      <FormularioCompra />
     </Container>
   )
 }
